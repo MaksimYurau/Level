e5e7ca947567da6dbfd8cb843488745eb0d43e22
MaksimYurau <2004softwaredeveloper@gmail.com>
2022-01-28 00:08:01 +0300

    Updated project. Updated MainActivity.kt. Updated activity_main.xml. Updated colors.xml. Updated strings.xml. Updated themes.xml.

--- app/src/main/java/com/maksimyurau/android/level/MainActivity.kt
+++ app/src/main/java/com/maksimyurau/android/level/MainActivity.kt
@@ -1,29 +1,63 @@
 package com.maksimyurau.android.level
 
+import android.content.Context
+import android.graphics.Color
 import android.hardware.Sensor
-import android.hardware.Sensor.TYPE_ACCELEROMETER
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.hardware.SensorManager.SENSOR_DELAY_NORMAL
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var sManager: SensorManager
+    lateinit var sManager: SensorManager
+    private var magnetic = FloatArray(9)
+    private var gravity = FloatArray(9)
+
+    private var accrs = FloatArray(3)
+    private var magf = FloatArray(3)
+    private var values = FloatArray(3)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val tvSensor = findViewById<TextView>(R.id.tvSensor)
-        sManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        val sensor = sManager.getDefaultSensor(TYPE_ACCELEROMETER)
+        val lRotation = findViewById<LinearLayout>(R.id.lRotation)
+
+        sManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensor = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val sensor2 = sManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
         val sListener = object : SensorEventListener {
-            override fun onSensorChanged(sEvent: SensorEvent?) {
-                val value = sEvent?.values
-                val sData = "X: ${value?.get(0)}\nY: ${value?.get(1)}\nZ: ${value?.get(2)}"
-                tvSensor.text = sData
+            override fun onSensorChanged(event: SensorEvent?) {
+                when (event?.sensor?.type) {
+                    Sensor.TYPE_ACCELEROMETER -> accrs = event.values.clone()
+                    Sensor.TYPE_MAGNETIC_FIELD -> magf = event.values.clone()
+                }
+
+                SensorManager.getRotationMatrix(gravity, magnetic, accrs, magf)
+                val outGravity = FloatArray(9)
+                SensorManager.remapCoordinateSystem(
+                    gravity,
+                    SensorManager.AXIS_X,
+                    SensorManager.AXIS_Z,
+                    outGravity
+                )
+                SensorManager.getOrientation(outGravity, values)
+                val degree = values[2] * 57.2958f
+                val rotate = 270 + degree
+                lRotation.rotation = rotate
+                val rData = 90 + degree
+                val color = if (rData.toInt() == 0) {
+                    Color.GREEN
+                } else {
+                    Color.RED
+                }
+                lRotation.setBackgroundColor(color)
+                tvSensor.text = rData.toInt().toString()
+
             }
 
             override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
@@ -31,6 +65,7 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-        sManager.registerListener(sListener, sensor, SENSOR_DELAY_NORMAL)
+        sManager.registerListener(sListener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sManager.registerListener(sListener, sensor2, SensorManager.SENSOR_DELAY_NORMAL)
     }
 }
