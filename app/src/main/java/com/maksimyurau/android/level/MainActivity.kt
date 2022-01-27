f4b6ee7e2ebfcf13cb16482a2a864901a0bcd050
MaksimYurau <2004softwaredeveloper@gmail.com>
2022-01-27 23:23:46 +0300

    Updated project. Added vcs.xml. Updated misc.xml. Updated MainActivity.kt. Updated activity_main.xml.

--- app/src/main/java/com/maksimyurau/android/level/MainActivity.kt
+++ app/src/main/java/com/maksimyurau/android/level/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.maksimyurau.android.level
 
-import androidx.appcompat.app.AppCompatActivity
+import android.hardware.Sensor
+import android.hardware.Sensor.TYPE_ACCELEROMETER
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.SensorManager.SENSOR_DELAY_NORMAL
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sManager: SensorManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tvSensor = findViewById<TextView>(R.id.tvSensor)
+        sManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        val sensor = sManager.getDefaultSensor(TYPE_ACCELEROMETER)
+        val sListener = object : SensorEventListener {
+            override fun onSensorChanged(sEvent: SensorEvent?) {
+                val value = sEvent?.values
+                val sData = "X: ${value?.get(0)}\nY: ${value?.get(1)}\nZ: ${value?.get(2)}"
+                tvSensor.text = sData
+            }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+            }
+
+        }
+        sManager.registerListener(sListener, sensor, SENSOR_DELAY_NORMAL)
     }
 }
